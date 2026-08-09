b77d9dd18d1e87e66c09eeb4a187cf6f4b31f7cf
idontwanttobeanurseanymore <martaariasolmo@hotmail.com>
2026-08-09 12:45:40 +0200

    useProducts hook for data fetching. Fetch products from API service. Manage loading, error and products state. Handle async logic with useEffect. Connect Home to hook and render products list. Data logic and UI separattion improved

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,19 +1,28 @@
 import { useEffect, useState } from "react";
-import { getProducts } from "../services/productsService";
+import { getAllProducts } from "../services/productsService";
 
-const useProducts = () => {
+export default function useProducts() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    getProducts()
-      .then((data) => setProducts(data))
-      .catch((err) => setError(err))
-      .finally(() => setLoading(false));
+    async function fetchProducts() {
+      try {
+        setLoading(true);
+
+        const data = await getAllProducts();
+        setProducts(data);
+      } catch (err) {
+        setError("Error al cargar productos");
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchProducts();
   }, []);
 
   return { products, loading, error };
-};
-
-export default useProducts;
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useEffect } from "react";
-import { getAllProducts } from "../services/productsService";
+import useProducts from "../hooks/useProducts";
 
 export default function Home() {
-  useEffect(() => {
-    async function load() {
-      const products = await getAllProducts();
-      console.log(products);
-    }
+  const { products, loading, error } = useProducts();
+  if (loading) {
+    return <p className="home__loading">Cargando...</p>;
+  }
 
-    load();
-  }, []);
+  if (error) {
+    return <p className="home__error">{error}</p>;
+  }
   return (
     <div className="home">
       <h2 className="home__title">Home</h2>
       <ul className="home__list">
-        <li className="home__item">
-          <Link to="/product/1" className="home__link">
-            Ver producto 1
-          </Link>
-        </li>
-
-        <li className="home__item">
-          <Link to="/product/2" className="home__link">
-            Ver producto 2
-          </Link>
-        </li>
+        {products.map((product) => (
+          <li key={product.id} className="home__item">
+            {product.title}
+          </li>
+        ))}
       </ul>
     </div>
   );
